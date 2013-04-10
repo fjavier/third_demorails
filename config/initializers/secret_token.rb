16a5e12a8053046a44f141bd0961143e161ad3ea
francisco <fjavier.cgam@gmail.com>
2013-04-10 16:07:38 -0600

    Subida Inicial del Tercer Demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThirdDemo::Application.config.secret_token = '5d13e5395f94fef197f7a6f011516760ca56fabfb4469780869f3cf37ba7d6663d3572b3edc99c476536e9745ed97a8db0efa51adfb6be49fc15da0c816fe31d'
